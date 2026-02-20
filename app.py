71eaf0efe1b82e46e1d45f4ad815039cb10c8e1d
nguyentienhiep2009tb-collab <nguyentienhiep2009tb@gmail.com>
2026-02-20 15:48:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -122,4 +122,7 @@ def index():
     return render_template_string(HTML)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+import os
+
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
