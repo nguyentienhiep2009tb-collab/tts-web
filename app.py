39dcbd07e9f17dc773f20ba231f22c6b82ea3736
nguyentienhiep2009tb-collab <nguyentienhiep2009tb@gmail.com>
2026-02-20 19:34:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -78,4 +78,5 @@ def download(filename):
     return send_file(filepath)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
